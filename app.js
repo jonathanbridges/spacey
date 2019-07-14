418a9fe96867495af6be4463bbf1b23d7f84b0be
jonathanbridges <jbridges7@gmail.com>
2019-07-14 13:43:42 -0700

    set up express routing

--- app.js
+++ app.js
@@ -4,16 +4,25 @@ const db = require('./config/keys').mongoURI;
 const express = require("express");
 const app = express();
 
+const users = require("./routes/api/users");
+
+const bodyParser = require('body-parser');
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
+
 app.get("/", (req, res) => res.send("New Text"));
+app.use("/api/users", users);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
 
 
 
