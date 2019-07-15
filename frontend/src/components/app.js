a550d837eeda8c9a358a2c82ae20260f3f1d786a
thethirdheat <rockstrongo22@gmail.com>
2019-07-15 15:39:01 -0700

    Styling Main Page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -8,14 +8,36 @@ import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 
-const App = () => (
-  <div className="App">
-    <Switch>
-        <Route exact path="/" component={MainPage} />
-        <AuthRoute exact path="/login" component={LoginFormContainer} />
-        <AuthRoute exact path="/signup" component={SignupFormContainer} />
-    </Switch>
-  </div>
-);
+class App extends React.Component{
+    constructor(props){
+    super(props)
+
+    this.state={
+      modal: 'close' 
+    }
+  }
+/* modal stuff
+        <button onClick={()=>this.setState({modal:'open'})}>this is a button</button>
+
+      <div className={`overlay${ this.state.modal==='open'?"--open" :""}`} onClick={()=>this.setState({modal: 'close'})}>
+        <div className={`modal${ this.state.modal==='open'?"--open" :""}`}>
+          <h3>this is stuff</h3>
+          <p>mosdlj fasdjkfasd fasdj;l fasdjkl;fadsre stuff</p>
+        </div>
+      </div>
+      */
+
+  render(){
+    return (
+    <div className="App">
+
+
+      <Switch>
+          <Route exact path="/" component={MainPage} />
+          <AuthRoute exact path="/login" component={LoginFormContainer} />
+          <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      </Switch>
+    </div>)}
+};
 
 export default App;
--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -4,17 +4,9 @@ import React from 'react';
 import NavBarContainer from '../nav/navbar_container';
 
 class MainPage extends React.Component {
-  constructor(props){
-    super(props)
-
-    this.state={
-      modal: 'close' 
-    }
-  }
 
 
   render() {
-    console.log('htis is bmodal', this.state.modal)
     return (
       <div className="MainPage">
 
@@ -27,18 +19,11 @@ class MainPage extends React.Component {
           </div>
           <div className="MainPage--Widgets">
             stuff here
-            <button onClick={()=>this.setState({modal:'open'})}>this is a button</button>
           </div> 
         </div>
 
 
-        <div className={`overlay${ this.state.modal==='open'?"--open" :""}`} onClick={()=>this.setState({modal: 'close'})}>
-          <div className={`modal${ this.state.modal==='open'?"--open" :""}`}>
-            <h3>this is stuff</h3>
-            <p>mosdlj fasdjkfasd fasdj;l fasdjkl;fadsre stuff</p>
-          </div>
-        </div>
-
+  
 
       </div>
     );
