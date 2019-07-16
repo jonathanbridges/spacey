bd8308a4cc8f9c18bb5a1309dac583615ce644cd
thethirdheat <rockstrongo22@gmail.com>
2019-07-16 09:53:14 -0700

    Create Modal actions

--- frontend/src/actions/ui_actions.js
+++ frontend/src/actions/ui_actions.js
@@ -2,6 +2,9 @@ export const TURN_ON_NASAIOTD = "TURN_ON_NASAIOTD"
 export const TURN_OFF_NASAIOTD = "TURN_OFF_NASAIOTD"
 
 
+export const TURN_ON_MODAL = "TURN_ON_MODAL"
+export const TURN_OFF_MODAL = "TURN_OFF_MODAL"
+
 const turnOnNiotd = () => ({
   type: 'TURN_ON_NASAIOTD',
 });
@@ -12,10 +15,24 @@ const turnOffNiotd = () => ({
 
 
 
+const turnOnModal = () => ({
+  type: 'TURN_ON_MODAL',
+});
+
+const turnOffModal = () => ({
+  type: 'TURN_OFF_MODAL',
+});
 
 export const dispatchTurnOnNiotd = () => dispatch => (
    dispatch(turnOnNiotd())
 )
 export const dispatchTurnOffNiotd = () => dispatch => (
    dispatch(turnOffNiotd())
+)
+
+export const dispatchTurnOn = () => dispatch => (
+   dispatch(turnOnModal())
+)
+export const dispatchTurnOff = () => dispatch => (
+   dispatch(turnOffModal())
 )
