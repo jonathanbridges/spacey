d777b7776c1222491933c93a7d3ad5f9ce632648
jonathanbridges <jbridges7@gmail.com>
2019-07-16 17:27:13 -0700

    Fix heroku issues

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 
