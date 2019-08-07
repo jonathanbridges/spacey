499387267974179f8b7a43aca5ac5fbf34498db0
Eric Leong <ericl.41@gmail.com>
2019-08-07 11:37:21 -0700

    Split AllNewsContainer into individual news widgets

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,87 +1,87 @@
 // src/components/main/main_page.js
 import React from 'react';
+import {connect} from 'react-redux';
 
-import NavBarContainer from '../nav/navbar_container';
-import NasaIOTD from '../widgets/nasa_iotd_container'
-import News from '../widgets/all_news';
-import RoverPhotos from '../widgets/rover_images_container'
+import { dispatchTurnOff, dispatchTurnOn } from '../../actions/ui_actions';
 
-import SideBar from '../sidebar/sidebar'
-import {connect} from 'react-redux'
+import LoginContainer from '../session/login_form_container';
+import SignUpContainer from '../session/signup_form_container';
 
-import LoginContainer from '../session/login_form_container'
-import SignUpContainer from '../session/signup_form_container'
+import NavBar from '../nav/navbar_container';
+import SideBar from '../sidebar/sidebar';
 
-import { dispatchTurnOff, dispatchTurnOn} from '../../actions/ui_actions'
+import HubbleNews from '../widgets/hubble_news_container';
+import NasaIOTD from '../widgets/nasa_iotd_container';
+import RoverPhotos from '../widgets/rover_images_container';
+import SpaceLaunchNews from '../widgets/space_launch_news_container';
+import TwitterFeed from '../widgets/twitter_feed_container';
+
 
 const mstp=(state,ownProps)=>({
   modal: state.ui.modal
-
 })
 
 const mdtp = (dispatch) =>({
   modalOn : (modal)=>dispatch(dispatchTurnOn(modal)),
   modalOff : ()=>dispatch(dispatchTurnOff())
-
 })
 
 
 class MainPage extends React.Component {
 
-
   render() {
-    let overlay  = ""
+    let overlay = ""
     if( this.props.modal!=='off'){
       if(this.props.modal==='signUp'){
        overlay= (<div className='overlay' >
           <div className="overlay--ClickGrabber"  onClick={()=>this.props.modalOff()} ></div>
           <SignUpContainer/>
        </div> )
-
       }else if( this.props.modal ==='login'){
        overlay= (<div className='overlay' >
           <div className="overlay--ClickGrabber"  onClick={()=>this.props.modalOff()} ></div>
           <LoginContainer/>
        </div> )
-
       }
     }
-
 
     return (
       <div className="MainPage">
         {overlay}
-        <NavBarContainer />
-        <div className ="MainPage--Body">
+        <NavBar />
+        <div className="MainPage--Body">
           <div className="MainPage--SideBar">
-            <SideBar/>
+            <SideBar />
           </div>
           <div className="MainPage--Widgets">
             <div className="top-widgets">
               <NasaIOTD />
-              <News />
+              <HubbleNews />
+              <SpaceLaunchNews />
+              <TwitterFeed />
             </div>
             <RoverPhotos />
           </div>
-          <div className="stars"></div>
-          <div className="twinkling"></div>
-          <div className="clouds"></div>
+          <div className="stars" />
+          <div className="twinkling" />
+          <div className="clouds" />
           <div className="night">
-            <div className="shooting_star"></div>
-            <div className="shooting_star"></div>
-            <div className="shooting_star"></div>
-            <div className="shooting_star"></div>
-            <div className="shooting_star"></div>
-            <div className="shooting_star"></div>
-            <div className="shooting_star"></div>
-            <div className="shooting_star"></div>
-            <div className="shooting_star"></div>
-            <div className="shooting_star"></div>
+            <div className="shooting_star" />
+            <div className="shooting_star" />
+            <div className="shooting_star" />
+            <div className="shooting_star" />
+            <div className="shooting_star" />
+            <div className="shooting_star" />
+            <div className="shooting_star" />
+            <div className="shooting_star" />
+            <div className="shooting_star" />
+            <div className="shooting_star" />
           </div>
         </div>
       </div>
     );
   }
+
 }
 
 export default connect(mstp,mdtp)(MainPage);
