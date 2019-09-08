ee2439b2df4d820b0413127680d0d9dd5025e406
jonathanbridges <jbridges7@gmail.com>
2019-09-08 10:22:43 -0700

    Add video support for NASA IOTD

--- frontend/src/components/widgets/nasa_iotd.jsx
+++ frontend/src/components/widgets/nasa_iotd.jsx
@@ -18,22 +18,30 @@ class imageOfTheDay extends React.Component {
   }
 
   render () {
-//    if(this.props.display==="off"){
-//      return null
-//    }
     if (this.props.nasaIOTD === undefined) {
       return (<div></div>)
     } else {
       let iotd = this.props.nasaIOTD.data;
-//
-      return (
-        <div className="NasaIotd">
-          <h1 className="NasaIotd--header">NASA Image of the Day</h1>
-          <p className="NasaIotd--copyright">Copyright: {iotd.copyright}</p>
-          <img className="NasaIotd--img" src={iotd.url} width="100%" height="auto" alt="" onClick={this.setModalImg()} />
-          <p className="NasaIotd--desc">{iotd.explanation}</p>
-        </div>
-      )
+      if (iotd.media_type === "video") {
+        return (
+          <div className="NasaIotd">
+            <h1 className="NasaIotd--header">NASA Image of the Day</h1>
+            <br/>
+            <iframe title="NASA Image of the Day" src={iotd.url} width="100%" height="300px" allow="fullscreen"></iframe>
+            <br/>
+            <p className="NasaIotd--desc">{iotd.explanation}</p>
+          </div>
+        )
+      } else {
+        return (
+          <div className="NasaIotd">
+            <h1 className="NasaIotd--header">NASA Image of the Day</h1>
+            <p className="NasaIotd--copyright">Copyright: {iotd.copyright}</p>
+            <img className="NasaIotd--img" src={iotd.url} width="100%" height="auto" alt="" onClick={this.setModalImg()} />
+            <p className="NasaIotd--desc">{iotd.explanation}</p>
+          </div>
+        )
+      }
     }
   }
 }
